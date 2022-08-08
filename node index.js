dc7b84614fda57c15db503e60a45efd3ac71a87c
sevilcankiz <kumral-sevil21@hotmail.de>
2022-08-08 18:15:29 +0200

    mixed message

---
+++ node index.js
@@ -0,0 +1,18 @@
+
+
+let kaffee = ['Ich sehe einen Rabben, du wirst eine Kluge Entscheidung treffen', 
+'Ich sehe einen Tornado, eine aufwirbelnde Nachricht wird dich erreichen', 
+'Ich sehe eine maske, jemand in deiner Umgebung spielt dir etwas vor', 
+'Ich sehe einen Wald, auf deiner Reise wirst du vielen Überraschungen begegnen'];
+
+alert(kaffee[Math.floor(Math.random() * kaffee.length)]);
+
+
+//function ClickAnswer() {
+//        alert(successMsg[Math.floor(Math.random() * successMsg.length)]);
+//
+//       location.reload();
+
+//   } else {
+//        alert(errorMsg[Math.floor(Math.random() * successMsg.length)]);
+//    }
